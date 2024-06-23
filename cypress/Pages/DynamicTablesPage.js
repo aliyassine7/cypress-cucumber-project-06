579c1040e5d21b90eb78269cbf83abb72b306758
aliyassine7 <aliyassine43@gmail.com>
2024-06-23 15:57:09 -0500

    added all steps

---
+++ cypress/Pages/DynamicTablesPage.js
@@ -0,0 +1,73 @@
+class DynamicTablesPage {
+  /*Locators */
+  getHeading() {
+    return cy.get(".is-size-3");
+  }
+
+  getTableHeaders() {
+    return cy.get("#product_table th");
+  }
+
+  getTableRows() {
+    return cy.get('tbody').children();
+  }
+
+  getTable() {
+    return cy.get("#product_table");
+  }
+
+  getModalHeading() {
+    return cy.get('#modal_title')
+  }
+
+  getButton(button) {
+    switch (button) {
+      case "ADD PRODUCT":
+        return cy.get("#add_product_btn");
+      case "X":
+        return cy.get(".delete");
+      case "SUBMIT":
+        return cy.get("#submit");
+      default:
+        throw new Error(`Invalid Button!`);
+    }
+  }
+
+  getLabel(label) {
+    switch (label) {
+      case "Please select the quantity":
+        return cy.get('[for="product_quantity"]');
+      case "Please enter the name of the product":
+        return cy.get('[for="product_name"]');
+      case "Please enter the price of the product":
+        return cy.get('[for="product_price"]');
+      default:
+        throw new Error(`Invalid Label!`);
+    }
+  }
+
+  getInputBox(box) {
+    switch (box) {
+      case "Quantity":
+        return cy.get("#quantity");
+      case "Product":
+        return cy.get("#product");
+      case "Price":
+        return cy.get("#price");
+      default:
+        throw new Error(`Invalid Input Box!`);
+    }
+  }
+
+  getTotal() {
+    return cy.get("#total_amount");
+  }
+
+  getLastRow() {
+    return cy.get('tbody').children().last();
+  }
+
+  /*Methods */
+}
+
+export default DynamicTablesPage;
--- cypress/e2e/step-definitions/dynamicTablesSteps.js
+++ cypress/e2e/step-definitions/dynamicTablesSteps.js
@@ -1,5 +1,81 @@
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import DynamicTablesPage from "../../Pages/dynamicTablesPage";
+
+const dynamicTablesPage = new DynamicTablesPage()
 
 Given(/^the user is on "([^"]*)"$/, (url) => {
-  cy.get(url)
+  cy.visit(url)
 });
+
+Then(/^the user should see the "([^"]*)" heading$/, () => {
+	dynamicTablesPage.getHeading().should('be.visible')
+});
+
+Then(/^the user should see the table with the headers below$/, (dataTable) => {
+	const arr = dataTable.rawTable.flat()
+
+  dynamicTablesPage.getTableHeaders().each(($el, index) => {
+    cy.wrap($el).should('have.text', arr[index])
+  })
+});
+
+Then(/^the user should see the table with the rows below$/, (dataTable) => {
+	const arr = dataTable.rawTable
+  
+	dynamicTablesPage.getTableRows().each(($el, outerIndex) => {
+    cy.wrap($el).children().each(($cell, innerIndex) => {
+      cy.wrap($cell).should('have.text', arr[outerIndex][innerIndex])
+    })
+  })
+});
+
+Then(/^the user should see the "([^"]*)" button is enabled$/, (button) => {
+	dynamicTablesPage.getButton(button)
+});
+
+Then(/^the user should see the "([^"]*)" text displayed$/, (text) => {
+	dynamicTablesPage.getTotal(text)
+});
+
+When(/^the user clicks on the "([^"]*)" button$/, (button) => {
+	dynamicTablesPage.getButton(button).click()
+});
+
+Then(/^the user should see the "([^"]*)" modal with its heading$/, () => {
+	dynamicTablesPage.getModalHeading().should('be.visible')
+});
+
+Then(/^the user should see the "([^"]*)" label$/, (label) => {
+	dynamicTablesPage.getLabel(label).should('have.text', label)
+});
+
+Then(/^the user should see the "([^"]*)" input box is enabled$/, (box) => {
+	dynamicTablesPage.getInputBox(box).should('be.enabled')
+});
+
+Then(/^the user should not see the "([^"]*)" modal$/, () => {
+	dynamicTablesPage.getModalHeading().should('not.exist')
+});
+
+Then(/^the user enters the "([^"]*)" as "([^"]*)"$/, (box, input) => {
+	dynamicTablesPage.getInputBox(box).type(input)
+});
+
+Then(/^the user should see the table with the new row below$/, (dataTable) => {
+  const arr = dataTable.rawTable.flat()
+
+  dynamicTablesPage.getLastRow().children().each(($el, index) => {
+    cy.wrap($el).should('have.text', arr[index])
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
